0856c0d05cccb295db6e826952a637e3a56f843b
gabri <gabri@LAPTOP-3BH11PJV.home>
2020-03-16 19:34:02 +0100

    package pojos created and prosthetic class

---
+++ src/pojos/prosthetic.java
@@ -0,0 +1,18 @@
+package pojos;
+
+import java.io.Serializable;
+
+public class prosthetic implements Serializable{
+
+	
+	
+	private Integer id;
+	private String type;
+	private String material;
+	private Float price;
+	private Object dimensions;
+	private Boolean failures;
+	private Integer numberFailures;
+
+
+	}
