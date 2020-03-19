d2c5ab16844d1e31de07a42c0122d8c247c72f01
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-03-19 14:06:45 +0100

    jdbc

---
+++ src/jdbc/SQLConnect.java
@@ -0,0 +1,25 @@
+package jdbc;
+import java.sql.*;
+
+public class SQLConnect {
+
+	public static void main(String args[]) {
+		try {
+			// Open database connection
+			Class.forName("org.sqlite.JDBC");
+			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/Prosthetic_DB.db");
+			c.createStatement().execute("PRAGMA foreign_keys=ON");
+			System.out.println("Database connection opened.");
+			
+			// Here is where I do things with the database
+			
+			// Close database connection
+			c.close();
+			System.out.println("Database connection closed.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
