9f25599f456d2f1587c9da070ac8dc466ae24ef8
marin <marin@MSI.home>
2020-03-23 17:53:19 +0100

    changes

--- src/db/inteface/PatientManager.java
+++ src/db/inteface/PatientManager.java
@@ -2,6 +2,7 @@ package db.inteface;
 import pojos.Patient;
 import java.sql.Date;
 //jeje
+
 public interface PatientManager {
 	public void register(Patient patient);
 	public void login(Patient patient);
