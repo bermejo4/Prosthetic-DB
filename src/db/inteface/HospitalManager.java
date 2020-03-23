d451b4984f6b98e51bc0a0fe8bb039a9294eec7d
gabri <gabri@LAPTOP-3BH11PJV.home>
2020-03-23 18:44:48 +0100

    Hospitalmanager

---
+++ src/db/inteface/HospitalManager.java
@@ -0,0 +1,11 @@
+package db.inteface;
+
+import pojos.*;
+import java.util.List;
+
+public interface HospitalManager {
+
+	public List<Prosthetic> showProsthetics();
+	public void buy(int hospital_id, int prosthetic_id);
+	
+}
