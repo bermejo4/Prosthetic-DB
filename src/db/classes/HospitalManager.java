6309c48d79e75531d195a1cd1a46173beb112ffc
gabri <gabri@LAPTOP-3BH11PJV.home>
2020-03-27 20:31:16 +0100

    UpdateHospitalManag

---
+++ src/db/classes/HospitalManager.java
@@ -0,0 +1,43 @@
+package db.classes;
+
+import db.inteface.*;
+import pojos.*;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HospitalManager implements HospitalManagerInterface {
+
+	public void register(Hospital hospital) {
+
+	}
+
+	public void login(Hospital hospital) {
+
+	}
+
+	public List<Prosthetic> showProsthetics() {
+	//we use this method to link a prosthetic to the catalogue of a hospital
+	List<Prosthetic> prostheticList = new ArrayList<Prosthetic>();
+					
+	//we get all the prosthetic availables 
+					
+	try {
+			String sql = "SELECT * FROM prosthetic";
+			PreparedStatement prep = c.prepareStatement(sql);
+			ResultSet rs = prep.executeQuery();
+			
+			while(rs.next()) {
+				Integer prosthId = rs.getInt(prosthId);
+				
+			}
+						
+	}
+		}
+
+	public void buy(int hospital_id, int prosthetic_id) {
+
+	}
+}
--- src/db/inteface/HospitalManagerInterface.java
+++ src/db/inteface/HospitalManagerInterface.java
@@ -3,7 +3,7 @@ package db.inteface;
 import pojos.*;
 import java.util.List;
 
-public interface HospitalManager {
+public interface HospitalManagerInterface {
 
 	public List<Prosthetic> showProsthetics();
 	public void buy(int hospital_id, int prosthetic_id);
