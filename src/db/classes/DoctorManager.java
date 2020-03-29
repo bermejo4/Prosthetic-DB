0060789142c5708be894e030a1775d1a52d22fee
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-03-29 12:50:57 +0200

    src

--- src/db/classes/DoctorManager.java
+++ src/db/classes/DoctorManager.java
@@ -7,38 +7,38 @@ import java.util.List;
 
 import db.inteface.*;
 
+public class DoctorManager implements DoctorManagerInterface {
 
-public class DoctorManager implements DoctorManagerInterface{
-	
 	private Connection c;
-	
+
 	public DoctorManager(Connection c) {
-		this.c=c;
+		this.c = c;
 	}
-	
+
 	public void register(Doctor doc) {
-	
+
 	}
+
 	public void login(Doctor doc) {
-		
+
 	}
-	
+
 	public void update(Doctor doc) {
-		
+
 	}
-	
+
 	public void addPatient(Patient pat) {
-		
+
 	}
-	
+
 	public void selectProsthetic(Prosthetic prost) {
-		
+
 	}
-	
+
 	public void assignProsthetic(Date date) {
-		
+
 	}
-	
+
 	public List<Patient> searchPatientByTelephone(float tel) {
 		//- searchPatientByTelephone( telephone:Float) : List<Patient>
 		//;
@@ -47,6 +47,28 @@ public class DoctorManager implements DoctorManagerInterface{
 		//search for all patients that fit the name
 		try {
 			String sql = "SELECT * FROM patient WHERE telephone LIKE ?";
+			PreparedStatement prep = c.prepareStatement(sql);
+			
+			ResultSet rs = prep.executeQuery();
+			
+			while(rs.next()) {
+				int id = rs.getInt("patient_id");
+				String name = rs.getString("name");
+				String lastname = rs.getString("lastname");
+				Date dob = rs.getDate("dob");
+				String address = rs.getString("address");
+				float telephone = rs.getFloat("telephone");
+				String gender = rs.getString("gender");
+				String problem = rs.getString("problem");
+				int doctor_id=rs.getInt("doc_id");
+				// create a new patient
+				Patient newpatient = new Patient(id,name,lastname,telephone,dob,gender, String problem,
+						String addres);
+				
+				
+				
+			}
+			
 			
 			
 		}catch(Exception e) {
