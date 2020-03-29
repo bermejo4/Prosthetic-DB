71ed6489ad5c0d66adc5d63a350e73bc20d2e004
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-03-29 14:48:47 +0200

    patient

--- src/pojos/Patient.java
+++ src/pojos/Patient.java
@@ -13,36 +13,34 @@ public class Patient implements Serializable {
 	private String name;
 	private String lastname;
 	private float telephone;
+	private Date dob;
 	private Date dof;
 	private String gender;
 	private String problem;
 	private String addres;
+	private Integer doctor_id;
 	
 	
 	public Patient() {
 		super();
 	}
 
-	public Patient(Integer id, String name, String lastname, float telephone, Date dof, String gender, String problem,
-			String addres) {
+	public Patient(Integer id, String name, String lastname, float telephone, Date dof, Date dob, String gender, String problem,
+			String addres, Integer doctor_id) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.lastname = lastname;
 		this.telephone = telephone;
+		this.dob=dob;
 		this.dof = dof;
 		this.gender = gender;
 		this.problem = problem;
 		this.addres = addres;
+		this.doctor_id=doctor_id;
 	}
 
 	
-
-	@Override
-	public String toString() {
-		return "Patient [id=" + id + ", name=" + name + ", lastname=" + lastname + ", telephone=" + telephone + ", dof="
-				+ dof + ", gender=" + gender + ", problem=" + problem + ", addres=" + addres + "]";
-	}
 
 	@Override
 	public int hashCode() {
@@ -51,6 +49,7 @@ public class Patient implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -67,6 +66,13 @@ public class Patient implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + ", lastname=" + lastname + ", telephone=" + telephone + ", dob="
+				+ dob + ", dof=" + dof + ", gender=" + gender + ", problem=" + problem + ", addres=" + addres
+				+ ", doctor_id=" + doctor_id + "]";
 	}
 
 	public Integer getId() {
@@ -107,7 +113,16 @@ public class Patient implements Serializable {
 	public void setTelephone(float telephone) {
 		this.telephone = telephone;
 	}
+	
+	
 
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
 
 	public Date getDof() {
 		return dof;
@@ -119,7 +134,7 @@ public class Patient implements Serializable {
 	}
 
 
-	public String isGender() {
+	public String getGender() {
 		return gender;
 	}
 
@@ -147,6 +162,15 @@ public class Patient implements Serializable {
 	public void setAddres(String addres) {
 		this.addres = addres;
 	}
+
+	public Integer getDoctor_id() {
+		return doctor_id;
+	}
+
+	public void setDoctor_id(Integer doctor_id) {
+		this.doctor_id = doctor_id;
+	}
+	
 	
 	
 	
