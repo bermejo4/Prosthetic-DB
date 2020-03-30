6c879a30d5715c927ffb9dab52dd365e100e1b43
gabri <gabri@LAPTOP-3BH11PJV.home>
2020-03-30 17:29:25 +0200

    modificadoHospitalManager con error

--- src/db/classes/HospitalManager.java
+++ src/db/classes/HospitalManager.java
@@ -12,9 +12,9 @@ import java.util.List;
 
 public class HospitalManager implements HospitalManagerInterface {
 	private Connection c;
-	
+
 	public HospitalManager(Connection c) {
-		this.c=c;
+		this.c = c;
 	}
 
 	public void register(Hospital hospital) {
@@ -26,23 +26,30 @@ public class HospitalManager implements HospitalManagerInterface {
 	}
 
 	public List<Prosthetic> showProsthetics() {
-	//we use this method to link a prosthetic to the catalogue of a hospital
-	List<Prosthetic> prostheticList = new ArrayList<Prosthetic>();
-					
-	//we get all the prosthetic availables 
-					
-	try {
+		// we use this method to link a prosthetic to the catalogue of a hospital
+		List<Prosthetic> prostheticList = new ArrayList<Prosthetic>();
+
+		// we get all the prosthetic availables
+
+		try {
 			String sql = "SELECT * FROM prosthetic";
 			PreparedStatement prep = c.prepareStatement(sql);
 			ResultSet rs = prep.executeQuery();
-			
-			while(rs.next()) {
-				Integer prosthId = rs.getInt(prosthId);
-				
+
+			while (rs.next()) {
+				Integer prosthId = rs.getInt("id");
+				String prosthType = rs.getString("type");
+				Prosthetic newProsthetic = new Prosthetic(prosthId, prosthType);
+				prostheticList.add(newProsthetic);
+
 			}
-						
-	}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
+		return prostheticList;
+	}
 
 	public void buy(int hospital_id, int prosthetic_id) {
 
