04cc19612cc11466a14f22ef0b6c0712c7f00c9a
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-04-20 00:32:50 +0200

    db_manager

--- src/db/classes/DBManager.java
+++ src/db/classes/DBManager.java
@@ -71,12 +71,12 @@ public class DBManager implements DBManagerInterface{
 			Statement stmt1 = c.createStatement();
 			String sql1 = "CREATE TABLE doctor "
 					+ "(doctor_id	INTEGER NOT NULL UNIQUE,"
-					+ "name 	TEXT,"
-					+ "department	TEXT,"
+					+ "name TEXT,"
+					+ "telephone TEXT,"
+					+ "department TEXT,"
 					+ "Hosp_id	INTEGER,"
-					+ "Date_of_fitting	DATE,"
 					+ "PRIMARY KEY('doctor_id'),"
-					+ "FOREIGN KEY('Hosp_id') REFERENCES hospital('hospital_id') ON DELETE SET NULL ON UPDATE CASCADE)";
+					+ "FOREIGN KEY('hospital_id') REFERENCES hospital('hospital_id') ON DELETE SET NULL ON UPDATE CASCADE)";
 			
 			stmt1.executeUpdate(sql1);
 			stmt1.close();
@@ -84,8 +84,9 @@ public class DBManager implements DBManagerInterface{
 			Statement stmt2 = c.createStatement();
 			String sql2 = "CREATE TABLE hospital "
 					+ "(hospital_id	INTEGER NOT NULL UNIQUE,"
-					+ "name 	TEXT,"
-					+ "location 	TEXT,"
+					+ "name TEXT,"
+					+ "location TEXT,"
+					+ "telephone TEXT,"
 					+ "patient_id	INTEGER,"
 					+ "PRIMARY KEY('hospital_id'))";
 			
@@ -93,55 +94,61 @@ public class DBManager implements DBManagerInterface{
 			stmt2.close();
 			
 			Statement stmt3 = c.createStatement();
-			String sql3 = "CREATE TABLE patient "
-					   + "patient_id INTEGER NOT NULL UNIQUE,"
-					   + "name    TEXT,"
-					   + "lastname        TEXT,"
-					   + "dob     DATE,"
-					   + "dof	  DATE,"
-					   + "address   TEXT,"
-					   + "telephone     TEXT, "
-					   + "gender	TEXT,"
-					   + "problem      TEXT, "
-					   + "doc_id     INTEGER, "
+			String sql3 = "CREATE TABLE patient"
+					   + "(patient_id INTEGER NOT NULL UNIQUE,"
+					   + "name TEXT,"
+					   + "lastname TEXT,"
+					   + "dob DATE,"
+					   + "dof DATE,"
+					   + "address TEXT,"
+					   + "telephone TEXT,"
+					   + "gender TEXT,"
+					   + "problem TEXT,"
+					   + "doctor_id INTEGER,"
 					   + "PRIMARY KEY('patient_id'),"
-					   + "FOREIGN KEY('doc_id') REFERENCES doctor(doctor_id)"
-					   + "ON DELETE SET NULL ON UPDATE CASCADE)";
+					   + "FOREIGN KEY('doctor_id') REFERENCES doctor(doctor_id) ON DELETE SET NULL ON UPDATE CASCADE)";
+			
 			stmt3.executeUpdate(sql3);
 			stmt3.close();
 			
 			Statement stmt4 = c.createStatement(); 
 			String sql4 = "CREATE TABLE prosthetic "
 					+ "(prosthetic_id	INTEGER NOT NULL UNIQUE,"
-					+ "material 	TEXT,"
-					+ "type		TEXT,"
-					+ "dimension	TEXT,"
-					+ "failures   TEXT,"
-					//falta el number_failures
-					+ "price 	REAL"
+					+ "material TEXT,"
+					+ "type TEXT,"
+					+ "dimension TEXT,"
+					+ "failures TEXT,"
+					+ "price REAL"
+					+ "available BOOLEAN"
 					+ "patient_id	INTEGER,"
-					+ "hospital_id 	INTEGER"
+					+ "hospital_id 	INTEGER,"
 					+ "PRIMARY KEY('prosthetic_id'),"
-					+ "FOREIGN KEY('Hospital_id') REFERENCES hospital('hospital_id') ON DELETE SET NULL ON UPDATE CASCADE)";
+					+ "FOREIGN KEY('hospital_id') REFERENCES hospital('hospital_id') ON DELETE SET NULL ON UPDATE CASCADE,"
+					+ "FOREIGN KEY(patient_id) REFERENCES patient('patient_id') ON DELETE SER NULL ON UPDATE CASCADE)";
 			
 			stmt4.executeUpdate(sql4);
 			stmt4.close();
 			
 			Statement stmt5 = c.createStatement(); 
-			String sql5 = "CREATE TABLE BiomedEng "
-					+ "(be_id		INTEGER NOT NULL UNIQUE,"
-					+ " name 		TEXT NOT NULL,"
-					+ " lastname	TEXT NOT NULL,"
-					+ " PRIMARY KEY('be_id')";
+			String sql5 = "CREATE TABLE biomedical_engineer"
+					+ "(be_id	INTEGER NOT NULL UNIQUE,"
+					+ "name 	TEXT,"
+					+ "lastname		TEXT,"
+					+ "telephone TEXT,"
+					//+ "gender	TEXT,"
+					//+ "speciality TEXT,"
+					//+ "worklocation TEXT"
+					+ "PRIMARY KEY('be_id'))";
+					//+ "FOREIGN KEY('Hospital_id') REFERENCES hospital('hospital_id') ON DELETE SET NULL ON UPDATE CASCADE)";
 			
 			stmt5.executeUpdate(sql5);
 			stmt5.close();
 			
 			Statement stmt6 = c.createStatement(); 
-			String sql6 = "CREATE TABLE M_M "
-					+ "(prost_id	INTEGER NOT NULL,"
+			String sql6 = "CREATE TABLE m_m"
+					+ "(prosthetic_id	INTEGER NOT NULL,"
 					+ "be_id INTEGER NOT NULL,"
-					+ "FOREIGN KEY('prost_id')REFERENCES prosthetic('prosthetic_id') ON DELETE SET NULL ON UPDATE CASCADE,"
+					+ "FOREIGN KEY('prosthetic_id')REFERENCES prosthetic('prosthetic_id') ON DELETE SET NULL ON UPDATE CASCADE,"
 					+ "FOREIGN KEY('be_id') REFERENCES biomedical_engineer('be_id') ON DELETE SET NULL ON UPDATE CASCADE)";
 			
 			stmt6.executeUpdate(sql6);
@@ -168,8 +175,11 @@ public class DBManager implements DBManagerInterface{
 			// Close database connection
 			//c.close();
 			//System.out.println("Database connection closed.");
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (SQLException e) {
+			if (e.getMessage().contains("already exists")) {
+			} else {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -197,6 +207,44 @@ public class DBManager implements DBManagerInterface{
 			e.printStackTrace();
 		}
 		return result;
+	}
+	public void DeleteTables() {
+		try {
+			
+			// Drop tables: begin
+			Statement stmt1 = c.createStatement();
+			String sql1 = "DROP TABLE doctor";
+			stmt1.executeUpdate(sql1);
+			stmt1.close();
+			Statement stmt2 = c.createStatement();
+			String sql2 = "DROP TABLE patient";
+			stmt2.executeUpdate(sql2);
+			stmt2.close();
+			Statement stmt3 = c.createStatement();
+			String sql3 = "DROP TABLE hospital";
+			stmt3.executeUpdate(sql3);
+			stmt3.close();
+			Statement stmt4 = c.createStatement();
+			String sql4 = "DROP TABLE prosthetic";
+			stmt4.executeUpdate(sql4);
+			stmt4.close();
+			Statement stmt5 = c.createStatement();
+			String sql5 = "DROP TABLE biomedical_engineer";
+			stmt5.executeUpdate(sql5);
+			stmt5.close();
+			Statement stmt6 = c.createStatement();
+			String sql6 = "DROP TABLE m_m";
+			stmt6.executeUpdate(sql6);
+			stmt6.close();
+			System.out.println("Tables removed.");
+			// Drop tables: end
+			
+			// Close database connection
+			c.close();
+			System.out.println("Database connection closed.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 
