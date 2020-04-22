e848d79b5ef5ec6736bcc6c7e973b3dd2140152f
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-04-22 19:21:57 +0200

    db_managerInterface -> initializeDoctor

--- src/db/inteface/DBManagerInterface.java
+++ src/db/inteface/DBManagerInterface.java
@@ -5,6 +5,7 @@ public interface DBManagerInterface {
 	public void disconnect();
 	public void createTables();
 	public void deleteTables();
+	public void initializeDoctors(String name, String telephone, String department, int hospital_id);
 	
 	public DoctorManagerInterface getDoctorManager();
 	public HospitalManagerInterface getHospitalManager();
