32b9c6e531cc341b1bd5df7ec517a6bf1c1f16a8
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-04-29 00:46:43 +0200

    UserManagerInterface

--- src/db/inteface/UserManagerInterface.java
+++ src/db/inteface/UserManagerInterface.java
@@ -1,5 +1,8 @@
 package db.inteface;
 
+import java.util.List;
+
+import pojos.Role;
 import pojos.User;
 
 public interface UserManagerInterface {
@@ -7,6 +10,9 @@ public interface UserManagerInterface {
 	public void disconnect();
 	public void createUser(User user);
 	public int checkPassword(String username, byte[] password, String table, String name_id);
+	Role getRole(int id);
+	void createRole(Role role);
+	List<Role> getRoles();
 	
 
 }
