4535a42e662ebd015e5fcee5b9558d8b13bbaaca
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-05-15 20:52:03 +0200

    ui

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -420,6 +420,36 @@ public class Menu {
 		}
 	}
 	
+	/*private static void generateHtmlProsthetic() {
+		BufferedWriter bw = null;
+	    FileWriter fw = null;
+	    try {
+	        String data = "";
+	        File file = new File("./Prosthetic.html");
+	        // Si el archivo no existe, se crea!
+	        if (!file.exists()) {
+	            file.createNewFile();
+	        }
+	        // flag true, indica adjuntar información al archivo.
+	        fw = new FileWriter(file.getAbsoluteFile(), true);
+	        bw = new BufferedWriter(fw);
+	        bw.write(data);
+	        System.out.println("información agregada!");
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	                        //Cierra instancias de FileWriter y BufferedWriter
+	            if (bw != null)
+	                bw.close();
+	            if (fw != null)
+	                fw.close();
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	        }
+	    }
+	}*/
+	
 	public static Prosthetic uploadProsthetic() throws Exception {
 
 		System.out.println("Introduce the new Prosthetic: "); 
