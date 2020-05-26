18f5eb1314fd1ccc87206ad951e8659ac87f9cf3
Jaime Bermejo <jaimebermejotorres@gmail.com>
2020-05-26 12:59:38 +0200

    pojos4xml

--- src/pojos/pojos4Xml/SuperDBListing.java
+++ src/pojos/pojos4Xml/SuperDBListing.java
@@ -1,5 +1,50 @@
 package pojos.pojos4Xml;
 
-public class SuperDBListing {
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name = "DataBase")
+@XmlAccessorType (XmlAccessType.FIELD)
+public class SuperDBListing {
+	
+	@XmlElement
+	HospitalsListing hospitals; 
+	@XmlElement
+	BiomedicalsListing biomedicals;
+	@XmlElement
+	ProstheticsListing prosthetics;
+ 
+public SuperDBListing(HospitalsListing hospitals, BiomedicalsListing biomedicals, ProstheticsListing prosthetics) {
+		super();
+		this.hospitals = hospitals;
+		this.biomedicals = biomedicals;
+		this.prosthetics = prosthetics;
+	}
+
+public SuperDBListing() {
+	super();
+}
+
+public BiomedicalsListing getBiomedicals() {
+	return biomedicals;
+}
+public HospitalsListing getHospitals() {
+	return hospitals;
+}
+public ProstheticsListing getProsthetics() {
+	return prosthetics;
+}
+public void setBiomedicals(BiomedicalsListing biomedicals) {
+	this.biomedicals = biomedicals;
+}
+public void setHospitals(HospitalsListing hospitals) {
+	this.hospitals = hospitals;
+}
+public void setProsthetics(ProstheticsListing prosthetics) {
+	this.prosthetics = prosthetics;
+}
+ 
+ 
 }
